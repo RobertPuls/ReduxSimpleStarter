fbfca3aa24d40a798910f2d70ff62226c022372d
Puls <cg3t@M-C02F2DDFD59.nordstrom.net>
2017-10-31 16:12:18 -0700

    react done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/searchbar';
+import VideoList from './components/videoList';
+import VideoDetail from './components/videoDetail';
 
 const API_KEY = 'AIzaSyADquz_f3Ov8D_j3ujCFOu8kj9B-rvaZ70';
 
@@ -10,17 +13,33 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {videos: []};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({key: API_KEY, term: 'dog'}, (videos) => {
-      this.setState({videos});
+    this.onVideoSearch('surfboard')
+  }
+
+  onVideoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
+    const videoSearch = _.debounce(term => {this.onVideoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearch={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
